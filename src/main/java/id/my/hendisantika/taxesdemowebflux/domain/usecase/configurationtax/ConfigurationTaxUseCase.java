12b68cbe26ba5d53d523fb53390145de0fe8cc7a
hendisantika <hendisantika@yahoo.co.id>
2025-08-09 13:32:59 +0700

    feat: Add ConfigurationTaxUseCase🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/domain/usecase/configurationtax/ConfigurationTaxUseCase.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.taxesdemowebflux.domain.usecase.configurationtax;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/08/25
+ * Time: 13.32
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConfigurationTaxUseCase implements IConfigurationTax {
+}

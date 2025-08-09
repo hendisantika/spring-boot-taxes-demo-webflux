7b0626124b25220e82433edc2526959481643ab8
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:50:23 +0700

    feat: Add ExceptionUtil🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/in/web/util/ExceptionUtil.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.taxesdemowebflux.infrastructure.in.web.util;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 05.50
+ * To change this template use File | Settings | File Templates.
+ */
+@UtilityClass
+public class ExceptionUtil {
+    private static final String COLON = ":";
+}

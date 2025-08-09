2e49855d461ef9d4d95f8345916245655cbeab92
hendisantika <hendisantika@yahoo.co.id>
2025-08-09 13:39:38 +0700

    feat: Add processMessage in ProcessMessageUseCase🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/domain/usecase/processmessage/impl/ProcessMessageUseCase.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/domain/usecase/processmessage/impl/ProcessMessageUseCase.java
@@ -2,6 +2,7 @@ package id.my.hendisantika.taxesdemowebflux.domain.usecase.processmessage.impl;
 
 import id.my.hendisantika.taxesdemowebflux.domain.usecase.processmessage.IProcessMessageUseCase;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +19,16 @@ import lombok.RequiredArgsConstructor;
 public class ProcessMessageUseCase implements IProcessMessageUseCase {
 
     private final IMessageRepositoryPort messageRepositoryPort;
+
+    /**
+     * Process the message.
+     *
+     * @param messageModel the message model
+     * @return the mono
+     */
+    @Override
+    public Mono<Void> processMessage(MessageModel messageModel) {
+        return this.messageRepositoryPort.saveMessage(messageModel)
+                .then();
+    }
 }

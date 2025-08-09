5cded39b39debc70cf4ed0652f4946bc0fb929df
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:14:26 +0700

    feat: Add IEventPublisherPort🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/domain/model/events/port/IEventPublisherPort.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.taxesdemowebflux.domain.model.events.port;
+
+import id.my.hendisantika.taxesdemowebflux.domain.model.events.EventModel;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 05.14
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IEventPublisherPort {
+    <T> Mono<Void> emit(EventModel<T> event);
+
+    <T> Mono<Void> emit(String name, String eventId, T data);
+
+    <T> Mono<Void> emitOtherRouterKey(EventModel<T> event);
+}

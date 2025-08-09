5cbe0a94c8d0d25c8b28a38152256429884a6732
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:48:44 +0700

    feat: Add HolidayMapper🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/in/web/holiday/mapper/HolidayMapper.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.taxesdemowebflux.infrastructure.in.web.holiday.mapper;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 05.48
+ * To change this template use File | Settings | File Templates.
+ */
+@UtilityClass
+public class HolidayMapper {
+}

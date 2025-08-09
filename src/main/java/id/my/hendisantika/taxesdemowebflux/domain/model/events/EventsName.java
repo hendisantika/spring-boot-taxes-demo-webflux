98d0645a83df6bbe95983a872dc9bd7408f34891
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:12:12 +0700

    feat: Add EventsName🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/domain/model/events/EventsName.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.taxesdemowebflux.domain.model.events;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 05.11
+ * To change this template use File | Settings | File Templates.
+ */
+public enum EventsName {
+    TAX_EVENT_MESSAGE,
+    TAX_EVENT_OTHER,
+}

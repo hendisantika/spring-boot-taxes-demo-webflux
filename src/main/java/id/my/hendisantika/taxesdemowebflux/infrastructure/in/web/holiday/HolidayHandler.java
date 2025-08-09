2c67ed4ceccc074edced81c51e9c46db0833b995
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:45:27 +0700

    feat: Add getHoliday🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/in/web/holiday/HolidayHandler.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/in/web/holiday/HolidayHandler.java
@@ -2,10 +2,16 @@ package id.my.hendisantika.taxesdemowebflux.infrastructure.in.web.holiday;
 
 import id.my.hendisantika.taxesdemowebflux.domain.usecase.holiday.IHolidayExternUseCase;
 import id.my.hendisantika.taxesdemowebflux.domain.usecase.holiday.IHolidayUseCase;
+import id.my.hendisantika.taxesdemowebflux.infrastructure.in.web.commons.JsonApiDTO;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +32,17 @@ public class HolidayHandler {
     private final IHolidayUseCase holidayUseCase;
     private final IHolidayExternUseCase holidayExternUseCase;
     private final RequestValidator requestValidator;
+
+    public Mono<ServerResponse> getHoliday(ServerRequest serverRequest) {
+        //var result = holidayUseCase.getHolidays().collectList();
+        //return ServerResponse.ok().body(result, HolidayModel.class);
+        return Mono.just(serverRequest)
+                .map(rq -> holidayUseCase.getHolidays())
+                .flatMapMany(Function.identity())
+                .collectList()
+                .map(HolidayMapper::buildResponse)
+                .map(JsonApiDTO::new)
+                .flatMap(ServerResponse.ok()::bodyValue)
+                .onErrorResume(Mono::error);
+    }
 }

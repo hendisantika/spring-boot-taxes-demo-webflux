175bfe4e2d1ae22a3f1da666b55ccf6b7fd3ac75
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:31:55 +0700

    feat: Add RabbitMQConsumer🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/in/event/handler/RabbitMQConsumer.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.taxesdemowebflux.infrastructure.in.event.handler;
+
+import id.my.hendisantika.taxesdemowebflux.domain.usecase.processmessage.IProcessMessageUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 05.31
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@DependsOn("queue")
+@RequiredArgsConstructor
+public class RabbitMQConsumer {
+    private static final Logger logger = Logger.getLogger(RabbitMQConsumer.class.getName());
+    private final IProcessMessageUseCase processMessageUseCase;
+}

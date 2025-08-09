4a1d8a67e4cc6f17b386ba8a533cbe4c5513efb9
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:32:19 +0700

    feat: Add receiveMessage🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/in/event/handler/RabbitMQConsumer.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/in/event/handler/RabbitMQConsumer.java
@@ -1,10 +1,15 @@
 package id.my.hendisantika.taxesdemowebflux.infrastructure.in.event.handler;
 
+import id.my.hendisantika.taxesdemowebflux.domain.model.events.DomainEventModel;
 import id.my.hendisantika.taxesdemowebflux.domain.usecase.processmessage.IProcessMessageUseCase;
 import lombok.RequiredArgsConstructor;
+import org.reactivecommons.api.domain.DomainEvent;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -24,4 +29,14 @@ import java.util.logging.Logger;
 public class RabbitMQConsumer {
     private static final Logger logger = Logger.getLogger(RabbitMQConsumer.class.getName());
     private final IProcessMessageUseCase processMessageUseCase;
+
+    @RabbitListener(queues = "#{@rabbitQueueName}")
+    public Mono<Void> receiveMessage(DomainEvent<DomainEventModel> event) {
+        logger.log(Level.INFO, "📩RabbitListener Mensaje recibido: {0}", new Object[]{event});
+
+        return this.processMessageUseCase.processMessage(
+                this.getMessageModel(event)
+        ).then();
+    }
+
 }

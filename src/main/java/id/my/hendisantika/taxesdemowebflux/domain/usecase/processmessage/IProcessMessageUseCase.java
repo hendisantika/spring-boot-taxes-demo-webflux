19dccc4393a90ab64d43d5667eff404347775c1c
hendisantika <hendisantika@yahoo.co.id>
2025-08-09 13:38:51 +0700

    feat: Add IProcessMessageUseCase🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/domain/usecase/processmessage/IProcessMessageUseCase.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.taxesdemowebflux.domain.usecase.processmessage;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/08/25
+ * Time: 13.38
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IProcessMessageUseCase {
+
+    Mono<Void> processMessage(MessageModel messageModel);
+}

50b705c49bf9ebf8da688ff7616d74b1c676789b
hendisantika <hendisantika@yahoo.co.id>
2025-08-10 05:24:57 +0700

    feat: Add MessageStatus🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/domain/model/messagedata/MessageStatus.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.taxesdemowebflux.domain.model.messagedata;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 10/08/25
+ * Time: 05.24
+ * To change this template use File | Settings | File Templates.
+ */
+public enum MessageStatus {
+    PENDING, // Message not yet processed
+    PROCESSED, // Message processed successfully
+    FAILED //
+}

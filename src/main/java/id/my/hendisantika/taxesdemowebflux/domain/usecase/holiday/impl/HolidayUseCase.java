459e23dc2af9276a79318c66dd39d1d443ccc64d
hendisantika <hendisantika@yahoo.co.id>
2025-08-09 13:34:58 +0700

    feat: Add HolidayUseCase🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/domain/usecase/holiday/impl/HolidayUseCase.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.taxesdemowebflux.domain.usecase.holiday.impl;
+
+import id.my.hendisantika.taxesdemowebflux.domain.usecase.holiday.IHolidayUseCase;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/08/25
+ * Time: 13.34
+ * To change this template use File | Settings | File Templates.
+ */
+@RequiredArgsConstructor
+public class HolidayUseCase implements IHolidayUseCase {
+
+    private final IHolidayRepositoryPort holidayRepositoryPort;
+}

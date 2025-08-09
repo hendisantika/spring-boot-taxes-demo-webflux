5e0325988123de3794f7936dc3265eea7c6bf880
hendisantika <hendisantika@yahoo.co.id>
2025-08-09 13:23:48 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-taxes-demo-webflux
---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/SpringBootTaxesDemoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.taxesdemowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTaxesDemoWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTaxesDemoWebfluxApplication.class, args);
+    }
+
+}

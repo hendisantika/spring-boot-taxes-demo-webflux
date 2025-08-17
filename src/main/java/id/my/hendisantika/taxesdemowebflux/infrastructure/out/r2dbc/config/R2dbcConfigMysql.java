120d1dbee9e252dd46990ba6657a9c49523184fc
hendisantika <hendisantika@yahoo.co.id>
2025-08-17 18:18:21 +0700

    feat: Add databaseClient in R2dbcConfigMysql🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/r2dbc/config/R2dbcConfigMysql.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/r2dbc/config/R2dbcConfigMysql.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -71,5 +72,10 @@ public class R2dbcConfigMysql {
     @Bean
     public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
         return new R2dbcTransactionManager(connectionFactory);
+    }
+
+    @Bean
+    public DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
+        return DatabaseClient.create(connectionFactory);
     }
 }

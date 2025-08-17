c2b01223ac05e3c9b61f4ec3783e3bc555be44fb
hendisantika <hendisantika@yahoo.co.id>
2025-08-17 18:19:13 +0700

    feat: Add HolidayRepositoryAdapter🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/r2dbc/holiday/adapter/HolidayRepositoryAdapter.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.taxesdemowebflux.infrastructure.out.r2dbc.holiday.adapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.taxesdemowebflux.domain.model.holiday.port.IHolidayRepositoryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/08/25
+ * Time: 18.18
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@RequiredArgsConstructor
+public class HolidayRepositoryAdapter implements IHolidayRepositoryPort {
+
+    private final HolidayRepository holidayRepository;
+    private final ObjectMapper objectMapper;
+}

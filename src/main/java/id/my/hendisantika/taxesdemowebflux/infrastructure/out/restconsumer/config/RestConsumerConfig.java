ef9e3f900970b432fa1f9c9df4ec763975c0e37c
hendisantika <hendisantika@yahoo.co.id>
2025-08-17 18:26:10 +0700

    feat: Add RestConsumerConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/restconsumer/config/RestConsumerConfig.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.taxesdemowebflux.infrastructure.out.restconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/08/25
+ * Time: 18.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class RestConsumerConfig {
+
+    private final int defaultConnectionTimeout;
+    private final int defaultReadTimeout;
+    private final int defaultWriteTimeout;
+}

6208eee52ef1ea78508bc04f324781c5de6c2c96
hendisantika <hendisantika@yahoo.co.id>
2025-08-17 18:17:50 +0700

    feat: Add connectionFactoryPool in R2dbcConfigMysql🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/r2dbc/config/R2dbcConfigMysql.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/r2dbc/config/R2dbcConfigMysql.java
@@ -1,11 +1,19 @@
 package id.my.hendisantika.taxesdemowebflux.infrastructure.out.r2dbc.config;
 
 import com.rabbitmq.client.ConnectionFactory;
+import dev.miku.r2dbc.mysql.MySqlConnectionConfiguration;
+import dev.miku.r2dbc.mysql.MySqlConnectionFactory;
+import dev.miku.r2dbc.mysql.constant.SslMode;
+import io.r2dbc.pool.ConnectionPool;
+import io.r2dbc.pool.ConnectionPoolConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.time.Duration;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,10 +33,36 @@ public class R2dbcConfigMysql {
     public ConnectionFactory connectionFactory(@Value("${r2dbc.mysql.url}") String url,
                                                @Value("${r2dbc.mysql.username}") String username,
                                                @Value("${r2dbc.mysql.password}") String password) {
-        //"r2dbc:mysql://localhost:3306/tu_base_de_datos"
+        //"r2dbc:mysql://localhost:3306/your_database"
         return ConnectionFactoryBuilder.withUrl(url)
                 .username(username)
                 .password(password)
                 .build();
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactoryPool(@Value("${r2dbc.mysql.host}") String host,
+                                                   @Value("${r2dbc.mysql.port}") Integer port,
+                                                   @Value("${r2dbc.mysql.username}") String username,
+                                                   @Value("${r2dbc.mysql.password}") String password,
+                                                   @Value("${r2dbc.mysql.database}") String database
+    ) {
+        MySqlConnectionFactory mysqlFactory = MySqlConnectionFactory.from(
+                MySqlConnectionConfiguration.builder()
+                        .host(host)
+                        .port(port)
+                        .username(username)
+                        .password(password)
+                        .database(database)
+                        .sslMode(SslMode.DISABLED)
+                        .build()
+        );
+        ConnectionPoolConfiguration poolConfig = ConnectionPoolConfiguration.builder(mysqlFactory)
+                .initialSize(5)  // Initial connections in the pool
+                .maxSize(20)     // Maximum connections in the pool
+                .maxIdleTime(Duration.ofMinutes(30)) // Maximum downtime
+                .validationQuery("SELECT 1") // Connection validation
+                .build();
+        return new ConnectionPool(poolConfig);
     }
 }

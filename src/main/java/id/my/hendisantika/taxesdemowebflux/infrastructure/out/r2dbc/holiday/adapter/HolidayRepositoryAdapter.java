9f171e816d887e48c946925ddd4c9d91334c096a
hendisantika <hendisantika@yahoo.co.id>
2025-08-17 18:19:31 +0700

    feat: Add getHolidays in HolidayRepositoryAdapter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/r2dbc/holiday/adapter/HolidayRepositoryAdapter.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/r2dbc/holiday/adapter/HolidayRepositoryAdapter.java
@@ -1,9 +1,11 @@
 package id.my.hendisantika.taxesdemowebflux.infrastructure.out.r2dbc.holiday.adapter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.taxesdemowebflux.domain.model.holiday.HolidayModel;
 import id.my.hendisantika.taxesdemowebflux.domain.model.holiday.port.IHolidayRepositoryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +24,11 @@ public class HolidayRepositoryAdapter implements IHolidayRepositoryPort {
 
     private final HolidayRepository holidayRepository;
     private final ObjectMapper objectMapper;
+
+    @Override
+    public Flux<HolidayModel> getHolidays() {
+        return holidayRepository.findAll()
+                .map(holiday -> objectMapper.map(holiday, HolidayModel.class));
+    }
+
 }

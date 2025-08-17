76910b59437fa479c4f27e6bdaa1b4c61b8e8f75
hendisantika <hendisantika@yahoo.co.id>
2025-08-17 18:28:40 +0700

    feat: Add ApiCalendarRequestBuilder🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/restconsumer/apicalendar/ApiCalendarRequestBuilder.java
@@ -0,0 +1,19 @@
+package id.my.hendisantika.taxesdemowebflux.infrastructure.out.restconsumer.apicalendar;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/08/25
+ * Time: 18.28
+ * To change this template use File | Settings | File Templates.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiCalendarRequestBuilder {
+}

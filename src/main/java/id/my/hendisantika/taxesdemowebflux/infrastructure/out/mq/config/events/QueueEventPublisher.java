72e91df5c96a163f77b2423dd687f4fbaccb6a4e
hendisantika <hendisantika@yahoo.co.id>
2025-08-17 18:12:21 +0700

    feat: Add emit in QueueEventPublisher🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/mq/config/events/QueueEventPublisher.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/mq/config/events/QueueEventPublisher.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.taxesdemowebflux.infrastructure.out.mq.config.events;
 
+import id.my.hendisantika.taxesdemowebflux.domain.model.events.EventModel;
 import id.my.hendisantika.taxesdemowebflux.domain.model.events.port.IQueuePublisherPort;
+import org.reactivecommons.api.domain.DomainEvent;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +30,13 @@ public class QueueEventPublisher implements IQueuePublisherPort {
                                @Value("${rabbitmq.queue-name}") String queueName) {
         this.rabbitTemplate = rabbitTemplate;
         this.queueName = queueName;
+    }
+
+
+    @Override
+    public <T> Mono<Void> emit(EventModel<T> event) {
+        DomainEvent<EventModel<T>> domainEvent = new DomainEvent<>(
+                DIRECT_QUEUE_NAME, event.getId(), event);
+        return Mono.fromRunnable(() -> rabbitTemplate.convertAndSend(queueName, domainEvent));
     }
 }

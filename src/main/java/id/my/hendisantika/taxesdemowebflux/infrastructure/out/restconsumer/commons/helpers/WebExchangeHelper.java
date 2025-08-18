a68951cac95997a8d5acf4636581abe440db2771
hendisantika <hendisantika@yahoo.co.id>
2025-08-19 06:18:52 +0700

    feat: Add WebExchangeHelper🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/restconsumer/commons/helpers/WebExchangeHelper.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.taxesdemowebflux.infrastructure.out.restconsumer.commons.helpers;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : taxes-demo-webflux
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/08/25
+ * Time: 06.18
+ * To change this template use File | Settings | File Templates.
+ */
+@UtilityClass
+public class WebExchangeHelper {
+}

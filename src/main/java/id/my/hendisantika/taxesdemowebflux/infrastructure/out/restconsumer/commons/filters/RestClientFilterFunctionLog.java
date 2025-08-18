927eeaf5d408014b2cb915da9b35357eca284472
hendisantika <hendisantika@yahoo.co.id>
2025-08-19 06:14:21 +0700

    feat: Add filter in RestClientFilterFunctionLog🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/restconsumer/commons/filters/RestClientFilterFunctionLog.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/restconsumer/commons/filters/RestClientFilterFunctionLog.java
@@ -1,7 +1,15 @@
 package id.my.hendisantika.taxesdemowebflux.infrastructure.out.restconsumer.commons.filters;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.logging.Logger;
 
@@ -22,4 +30,30 @@ public class RestClientFilterFunctionLog implements ExchangeFilterFunction {
     private static final Logger logger = Logger.getLogger(RestClientFilterFunctionLog.class.getName());
     private final String componentName;
 
+    @Override
+    public @NonNull Mono<ClientResponse> filter(@NonNull ClientRequest request, ExchangeFunction next) {
+        var requestSb = new StringBuilder();
+        var clientRequest = ClientRequest.from(request)
+                .body((out, context) -> request.body().insert(new ClientDecorator(out, requestSb), context))
+                .build();
+        return next.exchange(clientRequest)
+                .flatMap(response -> response.bodyToMono(DataBuffer.class)
+                        .defaultIfEmpty(createDefaultDataBuffer())
+                        .map(responseBuffer -> {
+                            var headersRes = response.headers().asHttpHeaders();
+                            if (response.statusCode().isError()) {
+                                writeErrorLog(clientRequest, responseBuffer, requestSb, headersRes);
+                            } else {
+                                writeSuccessfulLog(clientRequest, responseBuffer, requestSb, headersRes);
+                            }
+                            return ClientResponse.create(response.statusCode())
+                                    .headers(headers -> headers.addAll(headersRes))
+                                    .body(Flux.just(responseBuffer))
+                                    .build();
+                        }))
+                .onErrorResume(WebClientRequestException.class, ex -> {
+                    writeErrorLog(clientRequest, requestSb.toString(), ex);
+                    return Mono.error(ex);
+                });
+    }
 }

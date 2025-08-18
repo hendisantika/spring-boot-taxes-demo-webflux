0d858352edd35590de817cc033907d9bfe7a7d02
hendisantika <hendisantika@yahoo.co.id>
2025-08-19 06:07:48 +0700

    feat: Add REQUEST_ERROR_MAP in ErrorHandlerUtils🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/restconsumer/commons/exception/ErrorHandlerUtils.java
+++ src/main/java/id/my/hendisantika/taxesdemowebflux/infrastructure/out/restconsumer/commons/exception/ErrorHandlerUtils.java
@@ -1,6 +1,17 @@
 package id.my.hendisantika.taxesdemowebflux.infrastructure.out.restconsumer.commons.exception;
 
+import id.my.hendisantika.taxesdemowebflux.domain.model.exception.TechnicalException;
+import id.my.hendisantika.taxesdemowebflux.domain.model.exception.message.TechnicalExceptionMessage;
+import io.netty.handler.timeout.ReadTimeoutException;
 import lombok.NoArgsConstructor;
+
+import javax.net.ssl.SSLException;
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.security.cert.CertPathBuilderException;
+import java.util.Map;
+import java.util.function.Function;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,4 +26,14 @@ import lombok.NoArgsConstructor;
  */
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class ErrorHandlerUtils {
+    private static final Map<Class<? extends Throwable>, Function<Throwable, Exception>> REQUEST_ERROR_MAP =
+            Map.of(
+                    ReadTimeoutException.class, ex -> new TechnicalException(ex, TechnicalExceptionMessage.SERVICE_TIMEOUT),
+                    SocketTimeoutException.class, ex -> new TechnicalException(ex, TechnicalExceptionMessage.SERVICE_TIMEOUT),
+                    UnknownHostException.class, ex -> new TechnicalException(ex, TechnicalExceptionMessage.UNKNOWN_HOST_EXCEPTION),
+                    ConnectException.class, ex -> new TechnicalException(ex, TechnicalExceptionMessage.CONNECTION_SERVICE_EXCEPTION),
+                    SSLException.class, ex -> new TechnicalException(ex, TechnicalExceptionMessage.SSL_EXCEPTION),
+                    CertPathBuilderException.class, ex -> new TechnicalException(ex, TechnicalExceptionMessage.SSL_EXCEPTION)
+            );
+
 }
